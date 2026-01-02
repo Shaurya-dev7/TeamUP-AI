9b71691cf006a87ecee215f691a6b5873c3e7e4c
Shaurya-dev7 <shauryadeeprai@gmail.com>
2026-01-03 01:18:50 +0530

    fix: load supabase env variables safely

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,7 +1,6 @@
 import { cookies } from 'next/headers'
-
-import { createServerClient } from '@supabase/ssr'
-import { type Database } from '@/database.types';
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { type Database } from '@/database.types'
 
 export async function createClient() {
   const cookieStore = await cookies()
@@ -14,7 +13,7 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options: CookieOptions }[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
