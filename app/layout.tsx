07ddc7772dd2efd9d073dd9bc276112fd9103ed5
Shaurya-dev7 <shauryadeeprai@gmail.com>
2026-01-06 09:52:50 +0530

    feat: add SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import { Outfit, Plus_Jakarta_Sans } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import AppHeader from "@/components/AppHeader";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import "./globals.css";
@@ -44,6 +45,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           </div>
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
